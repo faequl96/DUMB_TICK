81cfa9c761aba6b58658b7675b3207b9b5c1b4e2
Faeq <faequl96@gmail.com>
2022-12-18 10:43:07 +0700

    ready to deploy FE

--- client/src/config/Api.js
+++ client/src/config/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-   baseURL: "https://dumbtick.up.railway.app/api/v1",
+   baseURL: process.env.REACT_APP_BASEURL,
 });
 
 export const setAuthToken = (token) => {
